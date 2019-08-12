77369efd10b94d04081bb28149f39efd1cc8ebbd
Roman Lynnyk <roman.lynnyk.sa@gmail.com>
2019-08-12 23:31:58 +0300

    Add files via upload

---
+++ epam_training_01/Area_of_rectangle/Circle_static.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Area_of_rectangle
+{
+    static class Circle_static
+    {
+        private static double Radius, area, length;
+        private const double pi = 3.14;
+        public static void SetRadius(double Radius1)
+        {
+            Radius = Radius1;
+        }
+        public static void Circle_length()
+        {
+            length = 2 * pi * Radius;
+        }
+        public static void Circle_area()
+        {
+            area = pi * (Radius * Radius);
+        }
+        public static void get_info()
+        {
+            if (Radius != 0)
+            {
+                Console.WriteLine("Radius is: " + Radius + " Length is: " + length + " area is: " + area);
+            }
+        }
+    }
+}
