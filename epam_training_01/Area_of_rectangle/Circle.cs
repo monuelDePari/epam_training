5b908c5fac10c4654eddd8ed2c6b22cf6ad99ef4
Roman Lynnyk <roman.lynnyk.sa.@gmail.com>
2019-08-20 15:56:31 +0300

    updated repo

---
+++ epam_training_01/Area_of_rectangle/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Area_of_rectangle
+{
+    class Circle
+    {
+        private double Radius, area, length;
+        private const double pi = 3.14;
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+            Circle_length(Radius);
+            Circle_area(Radius);
+        }
+        public void Circle_length(double Radius)
+        {
+            length = 2 * pi * Radius;
+        }
+        public void Circle_area(double Radius)
+        {
+            area = pi * (Radius * Radius);
+        }
+        public void get_info()
+        {
+            if(Radius != 0)
+            {
+                Console.WriteLine("Radius is: " + Radius + " Length is: " + length + " area is: " + area);
+            }
+        }
+    }
+}
